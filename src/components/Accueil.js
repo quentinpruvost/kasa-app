ea05edbc9162f87563df498d5fcb3b738c7ff576
quentinpruvost <quentinbar26@gmail.com>
2023-08-13 16:59:51 +0200

    setting up the style for the home page

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import appartementsData from '../data/appartements.json';
+import './styles/accueil/accueil.css'; // Importez le fichier CSS
 
 function Accueil() {
   return (
     <div>
-      <h1>Appartements de vacances</h1>
+      <div className="accueil-image-container">
+        <img
+          src="https://zupimages.net/up/23/32/xz1k.png"
+          alt="accueil"
+        />
+        <div className="image-text">
+          Chez vous, partout et ailleurs
+        </div>
+      </div>
       <ul>
         {appartementsData.map((appartement) => (
           <li key={appartement.id}>
